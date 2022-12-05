b2097a46572ea188e25adb0aa9883f0e836479f2
jgestradam <jgestradam@unal.edu.co>
2022-12-05 18:25:07 -0500

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Joseph Estrada on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

9ff8ebce6198425ee3ef49ee32ceeb42ee134be8
jgestradam <jgestradam@unal.edu.co>
2022-12-06 10:14:50 -0500

    Add a dynamic List to display downloaded coins

---
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  Crypto
+//
+//  Created by Joseph Estrada on 12/6/22.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+     
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+    
+}

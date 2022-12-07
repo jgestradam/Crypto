d876b98eaa5d94fe01d0176e4a5ea5a2b0450199
jgestradam <jgestradam@unal.edu.co>
2022-12-06 21:34:08 -0500

    Implement a custom and reusable search bar

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -22,6 +22,8 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                SearchBarView(searchText: $vm.searchText)
+                
                 columnTitles
                 
                 if (vm.allCoins.count != 0) {
